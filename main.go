32b8bd59eeb3cf9e4ca69d754b476301ae108f49
Awtanabe <panser098@gmail.com>
2025-03-10 23:14:53 +0900

    hello world

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	// ListenAndServe ホストを起動する
+	err := http.ListenAndServe(":18080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello world %s", r.URL.Path[1:])
+	}),
+)
+
+if err != nil {
+	fmt.Printf("failed to terminate server: %v", err)
+	os.Exit(1)
+}
+
+}
---
+++ memo.md
@@ -0,0 +1,5 @@
+### http server作成
+
+- main.go
+- ポート開ける
+- hello world
