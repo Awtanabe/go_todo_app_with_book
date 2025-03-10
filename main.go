53f3c8600b5ca5e3070a792293bfdf2d006e863f
Awtanabe <panser098@gmail.com>
2025-03-10 23:20:14 +0900

    pretter

--- main.go
+++ main.go
@@ -12,11 +12,11 @@ func main() {
 	err := http.ListenAndServe(":18080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello world %s", r.URL.Path[1:])
 	}),
-)
+	)
 
-if err != nil {
-	fmt.Printf("failed to terminate server: %v", err)
-	os.Exit(1)
-}
+	if err != nil {
+		fmt.Printf("failed to terminate server: %v", err)
+		os.Exit(1)
+	}
 
 }
